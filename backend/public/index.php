8c8684f4920c638bc08d85d15dca46fa8f2aab26
Krish120907 <krishmaurya7156@gmail.com>
2026-07-20 11:42:49 +0530

    submitted by updated

--- backend/public/index.php
+++ backend/public/index.php
@@ -133,8 +133,6 @@ switch ($resource) {
             } else {
                 $adminController->createProblem($user['id'], $body);
             }
-        } elseif ($method === 'GET' && $resourceId === 'submissions') {
-            $adminController->getAllSubmissions();
         } elseif ($method === 'GET' && $resourceId === 'problems') {
             $adminController->listProblems($params);
         } elseif ($method === 'PUT' && $resourceId === 'problems' && $subResource !== null) {
